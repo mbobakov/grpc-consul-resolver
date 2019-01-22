fa5c5141dc49ce3d0bdec09ce13257b82b36411c
ven <p.gulbin@fbs.com>
2019-01-22 14:02:18 +0300

    fix readme receive service address from node

--- consul.go
+++ consul.go
@@ -61,7 +61,7 @@ func watchConsulService(ctx context.Context, s servicer, tgt target, out chan<- 
 
 			ee := make([]string, 0, len(ss))
 			for _, s := range ss {
-				ee = append(ee, fmt.Sprintf("%s:%d", s.Service.Address, s.Service.Port))
+				ee = append(ee, fmt.Sprintf("%s:%d", s.Node.Address, s.Service.Port))
 			}
 			if tgt.Limit != 0 && len(ee) > tgt.Limit {
 				ee = ee[:tgt.Limit]
