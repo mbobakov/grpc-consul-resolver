3f28fed00fa464f3b73df4b60a9288e110681f75
Mstislav Bobakov <m.bobakov@gmail.com>
2020-01-07 17:06:10 +0100

    update dependencies

--- consul_test.go
+++ consul_test.go
@@ -5,48 +5,50 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/consul/api"
-	"github.com/pkg/errors"
+	"github.com/mbobakov/grpc-consul-resolver/internal/mocks"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/resolver"
 )
 
 func TestPopulateEndpoints(t *testing.T) {
 	tests := []struct {
-		name  string
-		input [][]string
-		want  []resolver.Address
+		name      string
+		input     [][]string
+		wantCalls [][]resolver.Address
 	}{
-		{"one", [][]string{{"127.0.0.1:50051"}}, []resolver.Address{{Addr: "127.0.0.1:50051"}}},
+		{"one",
+			[][]string{{"127.0.0.1:50051"}},
+			[][]resolver.Address{
+				[]resolver.Address{
+					{Addr: "127.0.0.1:50051"},
+				},
+			},
+		},
 		{"sorted",
 			[][]string{
 				{"227.0.0.1:50051", "127.0.0.1:50051"},
 			},
-			[]resolver.Address{
-				{Addr: "127.0.0.1:50051"},
-				{Addr: "227.0.0.1:50051"},
-			},
-		},
-		{"multy",
-			[][]string{
-				{"127.0.0.1:50051"},
-				{"127.0.0.1:50052"},
-				{"127.0.0.1:50053"},
-			},
-			[]resolver.Address{
-				{Addr: "127.0.0.1:50053"},
+			[][]resolver.Address{
+				[]resolver.Address{
+					{Addr: "127.0.0.1:50051"},
+					{Addr: "227.0.0.1:50051"},
+				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 			var (
-				got []resolver.Address
-				in  = make(chan []string, len(tt.input))
+				in = make(chan []string, len(tt.input))
 			)
-			fcc := NewFakeClientConnDefaultFatal(t)
-			fcc.NewAddressHook = func(cc []resolver.Address) {
-				got = cc
+
+			fcc := mocks.NewMockClientConn(ctrl)
+			for _, aa := range tt.wantCalls {
+				fcc.EXPECT().UpdateState(resolver.State{Addresses: aa}).Times(1)
 			}
 
 			ctx, cancel := context.WithCancel(context.Background())
@@ -56,8 +58,6 @@ func TestPopulateEndpoints(t *testing.T) {
 				in <- i
 			}
 			time.Sleep(time.Millisecond)
-			require.True(t, fcc.NewAddressCalledN(len(tt.input)))
-			require.Equal(t, tt.want, got)
 		})
 	}
 }
@@ -66,21 +66,27 @@ func TestWatchConsulService(t *testing.T) {
 	tests := []struct {
 		name             string
 		tgt              target
-		addr             []string // port increased with 1 per invocation
-		startPort        int
-		times            uint64
-		errorFromService bool
+		services         []*api.ServiceEntry
+		errorFromService error
 		want             []string
 	}{
-		{"simple", target{Service: "svc", Wait: time.Second}, []string{"127.0.0.1"}, 100, 3, false, []string{"127.0.0.1:102"}},
-		{"error", target{}, []string{"127.0.0.1"}, 100, 2, true, nil},
-		{"limit", target{Limit: 1}, []string{"127.0.0.1", "127.0.0.2", "127.0.0.3"}, 100, 1, false, []string{"127.0.0.1:100"}},
-		{"limitOver", target{Limit: 10}, []string{"127.0.0.1"}, 100, 1, false, []string{"127.0.0.1:100"}},
+		{"simple", target{Service: "svc", Wait: time.Second},
+			[]*api.ServiceEntry{
+				&api.ServiceEntry{
+					Service: &api.AgentService{Address: "127.0.0.1", Port: 1024},
+				},
+			},
+			nil,
+			[]string{"127.0.0.1:1024"},
+		},
+		// TODO: Add more tests-cases
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
 			var (
 				got []string
@@ -95,30 +101,38 @@ func TestWatchConsulService(t *testing.T) {
 					}
 				}
 			}()
-			fconsul := NewFakeservicerDefaultFatal(t)
-
-			fconsul.ServiceHook = func(s, tag string, hlz bool, q *api.QueryOptions) ([]*api.ServiceEntry, *api.QueryMeta, error) {
-				require.Equal(t, tt.tgt.Service, s)
-				require.Equal(t, tt.tgt.Tag, tag)
-				require.Equal(t, tt.tgt.Healthy, hlz)
-				require.Equal(t, tt.tgt.Wait, q.WaitTime)
-				if q.WaitIndex >= tt.times {
-					select {}
-				}
-				if tt.errorFromService {
-					return nil, &api.QueryMeta{LastIndex: q.WaitIndex + 1}, errors.New("Error")
-				}
-				var rr []*api.ServiceEntry
-				for _, a := range tt.addr {
-					rr = append(rr, &api.ServiceEntry{Service: &api.AgentService{Address: a, Port: tt.startPort + int(q.WaitIndex)}})
-				}
-				return rr, &api.QueryMeta{LastIndex: q.WaitIndex + 1}, nil
-			}
+			fconsul := mocks.NewMockservicer(ctrl)
+			fconsul.EXPECT().Service(tt.tgt.Service, tt.tgt.Tag, tt.tgt.Healthy, &api.QueryOptions{
+				WaitIndex:         0,
+				Near:              tt.tgt.Near,
+				WaitTime:          tt.tgt.Wait,
+				Datacenter:        tt.tgt.Dc,
+				AllowStale:        tt.tgt.AllowStale,
+				RequireConsistent: tt.tgt.RequireConsistent,
+			}).
+				Times(1).
+				Return(tt.services, &api.QueryMeta{LastIndex: 1}, tt.errorFromService)
+			fconsul.EXPECT().Service(tt.tgt.Service, tt.tgt.Tag, tt.tgt.Healthy, &api.QueryOptions{
+				WaitIndex:         1,
+				Near:              tt.tgt.Near,
+				WaitTime:          tt.tgt.Wait,
+				Datacenter:        tt.tgt.Dc,
+				AllowStale:        tt.tgt.AllowStale,
+				RequireConsistent: tt.tgt.RequireConsistent,
+			}).
+				Do(
+					func(svc string, tag string, h bool, opt *api.QueryOptions) ([]*api.ServiceEntry, *api.QueryMeta, error) {
+						if opt.WaitIndex > 0 {
+							select {}
+						}
+						return tt.services, &api.QueryMeta{LastIndex: 1}, tt.errorFromService
+					},
+				).Times(1).
+				Return(tt.services, &api.QueryMeta{LastIndex: 1}, tt.errorFromService)
 
 			go watchConsulService(ctx, fconsul, tt.tgt, out)
-			time.Sleep(15 * time.Millisecond)
+			time.Sleep(5 * time.Millisecond)
 
-			require.True(t, fconsul.ServiceCalledN(int(tt.times)))
 			require.Equal(t, tt.want, got)
 		})
 	}
--- go.mod
+++ go.mod
@@ -1,19 +1,21 @@
 module github.com/mbobakov/grpc-consul-resolver
 
+go 1.13
+
 require (
-	github.com/armon/go-metrics v0.3.0 // indirect
-	github.com/go-playground/form v3.1.3+incompatible
-	github.com/hashicorp/consul/api v1.1.0
-	github.com/hashicorp/go-msgpack v0.5.5 // indirect
-	github.com/hashicorp/memberlist v0.1.5 // indirect
-	github.com/jpillora/backoff v0.0.0-20180909062703-3050d21c67d7
+	github.com/go-playground/form v3.1.4+incompatible
+	github.com/golang/mock v1.1.1
+	github.com/google/btree v1.0.0 // indirect
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/jpillora/backoff v1.0.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/sirupsen/logrus v1.1.1
-	github.com/stretchr/testify v1.3.0
-	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c // indirect
-	golang.org/x/net v0.0.0-20190403144856-b630fd6fe46b // indirect
-	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
-	golang.org/x/sys v0.0.0-20190403152447-81d4e9dc473e // indirect
-	google.golang.org/grpc v1.2.1-0.20181011201045-c05280cc73cd
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+	google.golang.org/grpc v1.26.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
